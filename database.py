4f9a1545e32dd4a9dd9092c5ddfd291ad849d6aa
Vamsi Darsi <vamsidarsi10@gmail.com>
2025-03-29 00:11:47 +0530

    Add full project

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
-DB_URL = "mysql+pymysql://root:@localhost:3306/restapi"
+DB_URL = "mysql+pymysql://randomuser:ikcL1P6d6pkUuwOBLkTWzFDY3d6GwHow@dpg-cvjeoueuk2gs738ter20-a/db_restapi_so4z"
 engine = create_engine(DB_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
